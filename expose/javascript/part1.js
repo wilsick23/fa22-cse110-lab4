6b8ed260ec70423b4a8058af461722db16defd70
wilsick23 <108480710+wilsick23@users.noreply.github.com>
2023-04-30 08:39:10 -0700

    expose part 1 done

--- expose/javascript/part1.js
+++ expose/javascript/part1.js
@@ -1,1 +1,11 @@
+function sumValues(n1, n2, add) {
+    if (add) {
+        const result = 0;
+        result = n1 + n2;
+        console.log('values added: ', result);
+    } else return;
 
+    console.log('final result: ', result);
+}
+
+sumValues(10, 10, true);
